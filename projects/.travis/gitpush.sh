602506c21f88eb9e9c1c092f0266c23a6751f817
Rama Krishna <rkabrahpudi@adaequare.com>
2018-10-28 20:12:22 +0530

    ci(ci: Fixing version update): ci: Fixing version update

---
+++ projects/.travis/gitpush.sh
@@ -0,0 +1,11 @@
+echo "git push"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git config --global push.default current
+git stash
+git checkout master
+git stash pop
+git add package.json
+git add package-lock.json
+git commit -m "Auto-updating package release version ${GH_TOKEN}"
+git push https://${GH_TOKEN}@github.com/rkramakrishna17/taxilla-poc-library.git
